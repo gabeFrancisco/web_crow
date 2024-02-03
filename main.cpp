58db8c8cc53c46238ace52812cdf6ac615d47088
gabeFrancisco <gabegit2@gmail.com>
2024-02-02 22:15:56 -0300

    added lpqxx postgres connector and first test

--- main.cpp
+++ main.cpp
@@ -1,14 +1,51 @@
 #include <crow.h>
+#include <pqxx/pqxx>
 
 int main(int argc, char **argv)
 {
     crow::SimpleApp app;
+
+    // Base home route
     CROW_ROUTE(app, "/")
     ([]()
-    { 
+     { 
         auto page = crow::mustache::load_text("index.html");
-        return page; 
-    });
+        return page; });
+
+    // Dynamic route
+    CROW_ROUTE(app, "/message/<string>")
+    ([](std::string name)
+     { 
+        auto index = crow::mustache::load("name.html");
+        crow::mustache::context ctx({{"person", name}});
+        return index.render(ctx); });
+
+    CROW_ROUTE(app, "/add/<int>/<int>")
+    ([](int a, int b)
+     { return std::to_string(a + b); });
+
+    CROW_ROUTE(app, "/db")
+    (
+        []()
+        {
+            try
+            {
+                pqxx::connection conn{"postgresql://localhost/DoctorKey?user=postgres&password=1234"};
+                pqxx::work tx{conn};
+
+                pqxx::row r = tx.exec1(
+                    "select * from \"Users\" limit 1");
+
+                tx.commit();
+
+                return r[0].as<std::string>();
+            }
+            catch (const std::exception& e)
+            {
+                std::cerr << e.what() << std::endl;
+                return std::string("Error");
+            }
+        });
 
     app.port(1808)
         .multithreaded()
